69b2104977114bf57eb9c9c94d356525310bd17b
Olena <anyanyme@gmail.com>
2023-02-12 17:32:45 +0100

    update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,53 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const galleryLink = document.querySelector(".gallery__link");
+
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, galleryMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+   return galleryItems.map((galleryItem) => {
+     return `<div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          data-source="${galleryItem.original}"
+          alt="${galleryItem.description}"
+        />
+      </a>
+    </div>`;
+   }).join(``);
+   
+};
+
+gallery.addEventListener(`click`, onGalleryClick);
+
+
+
+function onGalleryClick(evt) {
+const target = evt.target;
+
+if (!target.classList.contains(`gallery__image`)) {
+return;
+}
+
+evt.preventDefault();
+
+const original = target.dataset.source;
+
+basicLightbox
+.create(
+`<img width="1400" height="900" src="${original}">`)
+.show();
+};
+    
+
+
+// console.log(galleryItems);
+
+
+
