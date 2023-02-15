c193e881255460a001a71bade40f310b8ca33e29
Olena <anyanyme@gmail.com>
2023-02-15 13:03:23 +0100

    updating 01 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 
 const gallery = document.querySelector(`.gallery`);
 const galleryLink = document.querySelector(".gallery__link");
@@ -9,8 +8,9 @@ const galleryMarkup = createGalleryCardsMarkup(galleryItems);
 gallery.insertAdjacentHTML(`beforeend`, galleryMarkup);
 
 function createGalleryCardsMarkup(galleryItems) {
-   return galleryItems.map((galleryItem) => {
-     return `<div class="gallery__item">
+  return galleryItems
+    .map((galleryItem) => {
+      return `<div class="gallery__item">
       <a class="gallery__link" href="${galleryItem.original}">
         <img
           class="gallery__image"
@@ -20,34 +20,36 @@ function createGalleryCardsMarkup(galleryItems) {
         />
       </a>
     </div>`;
-   }).join(``);
-   
-};
+    })
+    .join(``);
+}
 
 gallery.addEventListener(`click`, onGalleryClick);
 
-
-
 function onGalleryClick(evt) {
-const target = evt.target;
+  const target = evt.target;
+  evt.preventDefault();
 
-if (!target.classList.contains(`gallery__image`)) {
-return;
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const original = target.dataset.source;
+
+  const instance = basicLightbox.create(`
+    <img src="${original}" width="1400" height="900">
+`);
+
+  instance.show();
+
+  gallery.addEventListener(`keydown`, onClose);
+
+  function onClose(evt) {
+    // console.log(evt);
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
 }
 
-evt.preventDefault();
-
-const original = target.dataset.source;
-
-basicLightbox
-.create(
-`<img width="1400" height="900" src="${original}">`)
-.show();
-};
-    
-
-
 // console.log(galleryItems);
-
-
-
